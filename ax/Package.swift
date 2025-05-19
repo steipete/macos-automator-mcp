8bfffaa624e640d1a60252ef92840611a210f257
Peter Steinberger <steipete@gmail.com>
2025-05-20 01:56:10 +0200

    Change build script to make a universal binary

--- ax/Package.swift
+++ ax/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "x",
+            name: "ax",
             swiftSettings: [
                 .unsafeFlags(["-framework", "ApplicationServices", "-framework", "AppKit"])
             ]
--- ax/ax_runner.sh
+++ ax/ax_runner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Simple wrapper script to catch signals and diagnose issues
 
-exec ./.build/release/x "$@"
+exec ./ax "$@"
